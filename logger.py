0d163206e9c1105db9ef55addc84f4f458dcbd13
Gyanvi Bhardwaj <130824064+GyanviB19@users.noreply.github.com>
2025-06-16 15:33:18 -0700

    logger.py

--- logger.py
+++ logger.py
@@ -2,7 +2,8 @@
 from pymongo import MongoClient
 
 # Replace <your_password> with your actual MongoDB Atlas password before using
-MONGO_URI = "mongodb+srv://gyanvi19:<0p8CeByjVxfASemn>@cluster0.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URI = "mongodb+srv://gyanvi19:Gyanvi%402025@coldchainai.50wng0j.mongodb.net/?retryWrites=true&w=majority&appName=ColdchainAI"
+
 client = MongoClient(MONGO_URI)
 db = client["ColdchainAI"]
 collection = db["FreshScoreLogs"]
